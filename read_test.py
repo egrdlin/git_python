9a7f90b4949d6013de103a646b7879664cb57e26
Donald Lin <lind@mail.gvsu.edu>
2019-11-21 22:20:39 -0500

    using dictionary approach

--- read_test.py
+++ read_test.py
@@ -8,31 +8,34 @@ while True:
 
 
 file_contents = fhand.read() # read the whole file
-print(file_contents)
 file_contents = file_contents.strip()
-file_contents = file_contents.replace("\t","").replace("\t\t\t","").replace(" ","").replace("\n","") # remove new line characters and all white spaces
+file_contents = file_contents.replace("\t\t"," ").replace("\n","\t")
+file_contents = file_contents[20:]
+print(file_contents)
+#FIXME: construct dictionary --> sort dictionary --> Export dictionary -->
 
-file_contents = file_contents[15:]
-print("after strip:\n", file_contents)
+# file_contents = file_contents[20:]
+# print("after strip:\n", file_contents)
+# file_contents = file_contents.replace("\t","").replace("\t\t\t","").replace(" ","").replace("\n","") # remove new line characters and all white spaces
 
-char_index = list()
-file_contents_list = list()
+# char_index = list()
+# file_contents_list = list()
 
-for ind, char in enumerate(file_contents):
-    if char.isalpha():
-        # print(char, ind)
-        char_index.append(ind)
-print(char_index)
+# for ind, char in enumerate(file_contents):
+#     if char.isalpha():
+#         # print(char, ind)
+#         char_index.append(ind)
+# print(char_index)
 
-# for i in range(0,len(char_index)): # get all but last percentage
-#     current_ind = char_index[i-1]
-#     next_ind = char_index[i]
-#     temp = file_contents[current_ind+1:next_ind]
-#     print(temp)
+# # get the frequency into a list
+# for i in range(0, len(char_index)):
+#     if i+1 != len(char_index):
+#         first_index = char_index[i]
+#         second_index = char_index[i+1]
+#         print(file_contents[first_index+1:second_index])
+#     else:
+#         last_index = char_index[-1]
+#         print(file_contents[(last_index+1):])
 
-# start = char_index[len(char_index)-1]
-# last_val = file_contents[start+1:] # get last value
-# print(last_val)
-
-#TODO: sort list in alphabetic order 
+# #TODO: sort list in alphabetic order 
 
