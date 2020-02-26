5934f319439b0fc0b281c6409e96403b282bbcec
Donald Lin <don.lin@dematic.com>
2020-02-25 20:31:55 -0800

    update

--- ex_11_02.py
+++ ex_11_02.py
@@ -10,13 +10,19 @@ while True:
         print("Can't open the file")
         continue
     break
-    
+
+
+total = 0
+count = 0
 for line in fhand:
     line = line.rstrip()
     search_result = re.search('^New Revision', line) # true or false
     if search_result:
         #print(line)
-        res = [int(i) for i in line.split() if i.isdigit()] 
-        print(res)
-
+        res = [int(i) for i in line.split() if i.isdigit()]
+        res = sum(d * 10**i for i, d in enumerate(res[::-1]))  
+        total = total+res
+        count +=1
+        # print(res)
+print(total/count)
 #TODO: Capture the number from string
